9e2c015b5df91bd1c4bc2371fad9f82edfff79cd
duocphamngoc <phamngocduoc0880@gmail.com>
2019-06-25 10:09:30 +0700

    +add data into map

--- SpaceShooter/Classes/ResourceManager.cpp
+++ SpaceShooter/Classes/ResourceManager.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 #include "iostream"
 #include "string"
+#include "stdlib.h"
 using namespace std;
+using namespace ui;
 USING_NS_CC;
 
 static ResourceManager *r = nullptr;
@@ -28,9 +30,57 @@ void ResourceManager::Load()
 	fstream f;
 	f.open("Data.bin", ios::in);
 	string data;
+	int first;
+	string second,third, check;
+	int i = 0, j=0;
+	int check_button=0;
 	while (!f.eof())
 	{
+		i++;
 		getline(f, data);
+		check = *data.begin();
+		if (check == "#")
+		{
+			i = 0;
+			j++;
+			continue;
+		}
+		else if (i == 1)
+		{
+			data = data.substr(4,1);
+			first = stoi(data);
+			if (j == 2) check_button++;
+		}
+		else if (i==2 )
+		{
+			int pos = data.find_first_of("%s");
+			data = data.substr(pos + 2, 100);
+			second ="res" + data;
+			Sprite* s = Sprite::create(second);
+			m_sprites.insert(pair<int, Sprite*>(first, s));
+
+			if (j != 2) {
+				i = 0;
+			}
+			else {
+				i = 10000;
+				check_button++;
+				continue;
+			}
+			if (j >= 3) {
+				Label* label = Label::createWithTTF("",second,20);
+				m_labels.insert(pair<int, Label*>(first, label));
+			}
+		}	
+		 if (j == 2 && check_button == 2) {
+			int pos_ = data.find_first_of("%s");
+			data = data.substr(pos_ + 2, 100);
+			third ="res"+ data;
+			i = 0;
+			check_button = 0;
+			ui::Button* button = ui::Button::create(second,third);
+			m_buttons.insert(pair<int, Button*>(first, button));
+		}
 	}
 	f.close();
 }
--- SpaceShooter/Classes/ResourceManager.h
+++ SpaceShooter/Classes/ResourceManager.h
@@ -3,20 +3,20 @@
 #include "string"
 #include <map>
 #include "ui/CocosGUI.h"
-using namespace ui;
+
 USING_NS_CC;
 
 class ResourceManager {
 private:
 	char* m_dataFolderPath;
-	char* m_sprites;
-	char* m_buttons;
-	char* m_labels;
+	std::map <int, Sprite*> m_sprites;
+	std::map <int, ui::Button*> m_buttons;
+	std::map <int, Label*> m_labels;
 public: 
 	static ResourceManager* getInstance();
 	void Init();
 	void Load();
 	Sprite* GetSpriteById();
-	Button* GetButtonById();
+	ui::Button* GetButtonById();
 	Label* GetLabelById();
 };
